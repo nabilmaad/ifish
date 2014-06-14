4606c2782ee5d0dad0ca91cd2e71183b24f9b28f
Jessy Jess <jecornej@JECORNEJ-M-R03H.CISCO.COM>
2014-06-14 12:56:39 -0700

    Changed structure

--- config/application.coffee
+++ config/application.coffee
@@ -4,7 +4,7 @@
 steroids.config.name = "iFish"
 
 # ## Start Location
-steroids.config.location = "http://localhost/index.html"
+steroids.config.location = "http://localhost/welcome.html"
 
 # ## Tab Bar
 # steroids.config.tabBar.enabled = true
--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,15 +1,54 @@
 // Display the native navigation bar with the title "Hello World!"
-steroids.view.navigationBar.show("iFish!");
+steroids.view.navigationBar.show("iFish");
+
+// var loginText = null;
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   loginText = document.querySelector("#login-text");
+// });
+
+// var loginButton = new steroids.buttons.NavigationBarButton();
+// loginButton.title = "Log in";
+
+// var logoutButton = new steroids.buttons.NavigationBarButton();
+// logoutButton.title = "Log out";
+
+// loginButton.onTap = function() {
+//     loginText.textContent = "Welcome, Agnus M.!";
+//     steroids.view.navigationBar.update({
+//         buttons: {
+//           right: [loginButton]
+//         }
+//     });
+// };
+
+// logoutButton.onTap = function() {
+//     loginText.textContent = "Please log in.";
+//     steroids.view.navigationBar.update({
+//         buttons: {
+//           right: [loginButton]
+//         }
+//     });
+// };
+
+// steroids.view.navigationBar.update({
+//     //overrideBackButton: true,
+//     buttons: {
+//       right: [loginButton]
+//     }
+// });
 
 // Set the WebView background color to white (effective on iOS only)
 steroids.view.setBackgroundColor("#FFFFFF");
 
 function openLayer(location) {
   // Create a new WebView that...
-  webView = new steroids.views.WebView({ location: location });
+  webView = new steroids.views.WebView({ location: "views/" + location });
 
   // ...is pushed to the navigation stack, opening on top of the current WebView.
   steroids.layers.push({ view: webView });
+  //steroids.view.navigationBar.show("Sign up");
+  //steroids.view.navigationBar.update("iFish");
 }
 
 function openModal(location) {
